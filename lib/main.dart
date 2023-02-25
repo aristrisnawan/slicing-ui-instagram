1176244c71579d6fa68f9c6f19ba2b3ce63cf580
aristrisnawan <aristrisnawan35@gmail.com>
2023-02-25 22:30:55 +0700

    add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
+  final condition = true;
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
             // title: Text("cek"),
             actions: [
               Padding(
-                padding: const EdgeInsets.only(right: 24),
+                padding: const EdgeInsets.only(right: 23),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
@@ -43,9 +44,32 @@ class MyApp extends StatelessWidget {
                     SizedBox(
                       width: 20,
                     ),
-                    Image.asset(
-                      "assets/chat.png",
-                      width: 25,
+                    Stack(
+                      children: [
+                        Image.asset(
+                          "assets/chat.png",
+                          width: 25,
+                        ),
+                        condition
+                            ? Container()
+                            : Positioned(
+                                right: -4,
+                                top: -6,
+                                child: Container(
+                                  height: 20,
+                                  width: 20,
+                                  decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      shape: BoxShape.circle),
+                                  child: Center(
+                                    child: Text(
+                                      "2",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                      ],
                     )
                   ],
                 ),
@@ -53,7 +77,36 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
-        body: HomePage()
+        body: HomePage(),
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add_box_outlined), label: ""),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  "assets/video.png",
+                  width: 20,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.amber,
+                    ),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80"),
+                    )),
+                label: ""),
+          ],
+        ),
       ),
     );
   }
